929a42b1742e81d02f08c2dad1e8639d7491d01c
amarjeetsingh192 <125898800+amarjeetsingh192@users.noreply.github.com>
2023-03-14 19:46:13 +0530

    Add files via upload

---
+++ operator per.py
@@ -0,0 +1,104 @@
+##program1
+a=10
+b=2
+
+s=a**b
+print(s)
+
+##program2
+a = 10
+b = 4
+
+print("a & b =", a & b)
+
+print("a | b =", a | b)
+
+print("~a =", ~a)
+
+##PROGRAM 3
+
+a = 10
+b = -10
+
+print("a >> 1 =", a >> 1)
+print("b >> 1 =", b >> 1)
+
+a = 5
+b = -10
+
+print("a << 1 =", a << 1)
+print("b << 1 =", b << 1)
+
+##program4
+a=2
+b=a**a**a
+print(b)
+
+##program4
+a=20
+b=10
+c = 4
+d =2
+exp2 = a*b^c+d//2
+print(exp2)
+print(exp2>>1)
+
+##program 5
+expr = 10 + 20 * 30
+print(expr)
+
+##program 6
+a=10
+b=15
+c=2
+z=4
+
+resl=6^b+z//c**c+b&a
+print(resl)
+
+
+##program 7
+a=10
+b=5
+c= 4
+
+rel=a*b+c**2
+print(rel)
+
+##program 8
+
+a=10
+b=12
+c=10
+
+s=a&b-c*2
+print(s)
+
+
+##program 9
+
+
+z=200
+b=100
+c=50
+
+x=z|b&c
+print(x)
+
+##program 10
+
+a=20
+b=10
+c = 4
+d =2
+exp2 = a^b**c+d//2
+print(exp2)
+print(exp2<<1)
+
+##program 11
+a=10
+b=2
+c=7
+d=20
+f=a+b*c/d
+print(f)
